5412089c8cd471e864cce9a79808a91ff53df1e8
mehdiakacem <mehdiakacem20@gmail.com>
2023-10-31 14:38:22 -0400

    add dockerfile for mariadb

--- srcs/requirements/wordpress/tools/install_wp.sh
+++ srcs/requirements/wordpress/tools/install_wp.sh
@@ -5,5 +5,7 @@ chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 wp cli update
 wp core download --locale=en_US --allow-root
+wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST
+wp core install --allow-root --url=makacem.42.fr --title=Inception --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com
 
 exec "$@"
